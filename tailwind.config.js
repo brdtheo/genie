cae9b78ff61058c0c768a505cb97dc8f86d1e573
brdtheo <theo.billardey@gmail.com>
2022-05-23 20:04:37 +0200

    feat: add tailwindcss and concurrently

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      lemon: "#fffd64",
+      black: "#121212",
+      white: "#ffffff",
+    },
+  },
+  plugins: [],
+};
