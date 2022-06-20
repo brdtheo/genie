d3c7b82ceb03212c38432b408fb4fcbe0ebcbb46
brdtheo <theo.billardey@gmail.com>
2022-06-20 19:13:24 +0200

    fix: add compose to use devtools with sagas

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,16 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./rootReducer";
 
 export default function configureStore(initialState) {
   const sagaMiddleware = createSagaMiddleware();
 
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(sagaMiddleware)
+    composeEnhancers(applyMiddleware(sagaMiddleware))
   );
 
   store.runSaga = sagaMiddleware.run;
