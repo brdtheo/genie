afc427e366358832424fe0ca6e62c645a90d89c9
brdtheo <theo.billardey@gmail.com>
2022-06-22 20:02:10 +0200

    feat: remove container in search layout

--- src/ui/SearchLayout.js
+++ src/ui/SearchLayout.js
@@ -1,12 +1,9 @@
 import { RouterView } from "../lib/redux-saga-router";
-import Container from "./components/Container";
 
 function SearchLayout() {
   return (
     <main className="bg-black min-h-screen">
-      <Container>
-        <RouterView name="main" />
-      </Container>
+      <RouterView name="main" />
     </main>
   );
 }
