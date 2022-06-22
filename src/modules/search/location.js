2fde9980d8035e3b804fb25fbad160c8045d3627
brdtheo <theo.billardey@gmail.com>
2022-06-22 19:53:45 +0200

    feat: add query param to search route

--- src/modules/search/location.js
+++ src/modules/search/location.js
@@ -4,6 +4,7 @@ import { createLocationTemplate } from "../../lib/redux-saga-router";
 export default {
   search: createLocationTemplate({
     name: "search",
-    path: "/search",
+    params: ["query"], // Here you can also set up a dynamic params in route
+    path: "/search?q=:query",
   }),
 };
