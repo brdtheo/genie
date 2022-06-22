1e62fe93c36a1f3b6e0288d4d67c600455f4fa61
brdtheo <theo.billardey@gmail.com>
2022-06-22 20:01:51 +0200

    feat: handle search bar state in component

--- src/modules/home/components/HomePage.js
+++ src/modules/home/components/HomePage.js
@@ -1,68 +1,14 @@
 import React from "react";
-import * as yup from "yup";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
 
-import { useSimpleForm } from "../../../hooks/useSimpleForm";
 import SearchBar from "../../../ui/components/SearchBar";
-import { setLoading, setQueryResult } from "../homeActions";
-import { getSearchRequestURL } from "../../../api/helpers";
-import { setLocation } from "../../../lib/redux-saga-router/redux/actions";
-import * as L from "../../../locationTemplates";
-
-const schema = yup.object().shape({
-  query: yup.string().required("Please enter an artist/song name"),
-});
-
-const initialFormValues = {
-  query: "",
-};
 
 function Homepage() {
-  // eslint-disable-next-line no-unused-vars
-  const [formData, setFormField, setFormData, setFormError] =
-    useSimpleForm(initialFormValues);
-  const loading = useSelector((state) => state.homeReducer.loading);
-  const dispatch = useDispatch();
-
-  const handleSubmitForm = async (e) => {
-    e.preventDefault();
-    /**
-     * Handle errors (empty field, offline)
-     * Create loading state
-     * Retrieve data using axios
-     * Once data is retrieved, redirect to search page (saga)
-     */
-    try {
-      await schema.validate(formData.values, { abortEarly: false });
-      dispatch(setLoading(true));
-
-      const res = await axios({
-        method: "get",
-        url: getSearchRequestURL(formData.values.query),
-      });
-
-      dispatch(setLoading(false));
-      dispatch(setQueryResult(res.data.response.hits));
-      dispatch(setLocation(L.Search.search(), "PUSH"));
-    } catch (err) {
-      dispatch(setLoading(false));
-      setFormError("query", "", err.message);
-    }
-  };
-
   return (
     <div className="flex flex-col">
       <h1 className="text-5xl font-bold leading-none mb-2 text-white">Genie</h1>
       <q className="leading-none text-white">Song Lyrics &amp; Knowledge</q>
 
-      <SearchBar
-        loading={loading}
-        value={formData.values.query}
-        onChange={(e) => setFormField("query", e.target.value)}
-        handleSubmit={handleSubmitForm}
-        error={formData.errors.query}
-      />
+      <SearchBar />
     </div>
   );
 }
