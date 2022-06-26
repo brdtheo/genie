23e003a7c165c3bb145edcb8cc932e464213ba86
brdtheo <theo.billardey@gmail.com>
2022-06-26 17:20:34 +0200

    feat: add custom viewport height class

--- src/ui/HomeLayout.js
+++ src/ui/HomeLayout.js
@@ -1,18 +1,20 @@
+import { useSelector } from "react-redux";
+
 import Footer from "./components/Footer";
 import { RouterView } from "../lib/redux-saga-router";
 import LoadingBar from "./components/LoadingBar";
-import { useSelector } from "react-redux";
 
 function HomeLayout() {
   const loading = useSelector((state) => state.homeReducer.loading);
 
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col min-safe-h-screen">
       <main className="bg-black flex flex-grow justify-center items-center">
         {loading && <LoadingBar />}
 
         <RouterView name="main" />
       </main>
+
       <Footer />
     </div>
   );
