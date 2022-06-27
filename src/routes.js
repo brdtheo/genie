6c826f5d4537ef6f51547968718f6addab3a1988
brdtheo <theo.billardey@gmail.com>
2022-06-27 16:55:56 +0200

    feat: create 404 page

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,11 @@ import { prepareRoutes } from "./lib/redux-saga-router";
 import homeRoutes from "./modules/home/route";
 import newsRoutes from "./modules/news/route";
 import searchRoutes from "./modules/search/route";
+import notFoundRoute from "./modules/404/route";
 
-export default prepareRoutes([...homeRoutes, ...newsRoutes, ...searchRoutes]);
+export default prepareRoutes([
+  ...homeRoutes,
+  ...newsRoutes,
+  ...searchRoutes,
+  ...notFoundRoute,
+]);
