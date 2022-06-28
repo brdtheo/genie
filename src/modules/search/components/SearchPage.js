e1cf8979020c351785167b95ea175d9090e95adf
brdtheo <theo.billardey@gmail.com>
2022-06-28 10:33:28 +0200

    feat: update search layout

--- src/modules/search/components/SearchPage.js
+++ src/modules/search/components/SearchPage.js
@@ -1,25 +1,20 @@
 import React from "react";
 
 import Section from "../../../ui/components/Section";
-import Header from "../../../ui/components/Header";
 import SearchCardList from "./SearchCard/SearchCardList";
 import Container from "../../../ui/components/Container";
 
 function SearchPage() {
   return (
-    <>
-      <Header />
+    <Container>
+      <Section title="Songs" customStyle="pt-20 md:pt-28 pb-8 sm:pb-16">
+        <SearchCardList type="song" />
+      </Section>
 
-      <Container>
-        <Section title="Songs" customStyle="pt-20 md:pt-28 pb-8 sm:pb-16">
-          <SearchCardList type="song" />
-        </Section>
-
-        <Section title="Artists">
-          <SearchCardList type="artist" />
-        </Section>
-      </Container>
-    </>
+      <Section title="Artists">
+        <SearchCardList type="artist" />
+      </Section>
+    </Container>
   );
 }
 
--- src/ui/layouts/SearchLayout.js
+++ src/ui/layouts/SearchLayout.js
@@ -1,8 +1,11 @@
 import { RouterView } from "../../lib/redux-saga-router";
+import Header from "../../modules/search/components/Header";
 
 function SearchLayout() {
   return (
     <main className="bg-black min-h-screen">
+      <Header />
+
       <RouterView name="main" />
     </main>
   );
