b3d60a9b5f5923c753e10119851325371eec84eb
brdtheo <theo.billardey@gmail.com>
2022-06-28 10:16:30 +0200

    feat: refactor SearchCard component

---
+++ src/modules/search/components/SearchCard/SearchCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { MdVerified } from "react-icons/md";
+import { IoEye } from "react-icons/io5";
+
+import { parseViews } from "../../../../api/helpers";
+import SearchCardLoading from "./SearchCardLoading";
+
+export default function SearchCard(props) {
+  const loading = useSelector((state) => state.homeReducer.loading);
+  const { item, type, artistName, artistImage, artistIsVerified } = props;
+  const isTypeSong = type === "song";
+
+  const getImage = () =>
+    isTypeSong ? item.result.song_art_image_thumbnail_url : artistImage;
+
+  const getImageAlt = () =>
+    isTypeSong
+      ? `${item.result.title} by ${item.result.artist_names}`
+      : artistName;
+
+  const getHeading = () => (isTypeSong ? item.result.title : artistName);
+
+  return (
+    <>
+      {loading ? (
+        <SearchCardLoading />
+      ) : (
+        <article className="text-white w-full h-20 flex bg-white bg-opacity-5 hover:bg-opacity-10 transition-colors">
+          <figure className="w-20 h-20 overflow-hidden">
+            <img
+              src={getImage()}
+              alt={getImageAlt()}
+              className="w-full h-full"
+            />
+          </figure>
+
+          <div className="ml-4 py-2 flex-1 flex flex-col justify-between">
+            <div className={`${isTypeSong ? "" : "flex min-w-fit"}`}>
+              <h3 className="text-sm font-semibold leading-none text-ellipsis whitespace-nowrap overflow-hidden">
+                {getHeading()}
+              </h3>
+
+              {isTypeSong && (
+                <span className="text-sm text-white text-opacity-60 text-ellipsis whitespace-nowrap overflow-hidden w-44 md:w-full block md:inline">
+                  {item.result.artist_names}
+                </span>
+              )}
+
+              {artistIsVerified && (
+                <MdVerified className="text-lemon ml-1 text-sm" />
+              )}
+            </div>
+
+            {isTypeSong && (
+              <span className="flex align-bottom font-semibold text-xs text-white leading-none">
+                <IoEye className="mr-1" />
+                {parseViews(item.result.stats.pageviews)}
+              </span>
+            )}
+          </div>
+        </article>
+      )}
+    </>
+  );
+}
---
+++ src/modules/search/components/SearchCard/SearchCardLoading.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function SearchCardLoading() {
+  return (
+    <article className="w-full h-20 flex bg-white bg-opacity-5">
+      <div className="w-20 h-full animate-pulse bg-white bg-opacity-20"></div>
+
+      <div className="ml-4 py-2 flex-1">
+        <div className="animate-pulse bg-white bg-opacity-20 h-6 w-3/4 md:w-72 mb-2"></div>
+        <div className="animate-pulse bg-white bg-opacity-20 h-5 w-1/2 md:w-56"></div>
+      </div>
+    </article>
+  );
+}
