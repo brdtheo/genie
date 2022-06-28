668fadf5bde36191dd2c3c1dfdd59c553a747a41
brdtheo <theo.billardey@gmail.com>
2022-06-28 10:16:54 +0200

    feat: delete Snackbar component

--- src/modules/404/components/NotFound.js
+++ src/modules/404/components/NotFound.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Container from "../../../ui/components/Container";
 
+import Container from "../../../ui/components/Container";
 import Header from "../../../ui/components/Header";
 import Section from "../../../ui/components/Section";
 
@@ -11,7 +11,9 @@ function NotFound() {
 
       <Container>
         <Section title="Not found" customStyle="pt-20 md:pt-28 pb-8 sm:pb-16">
-          <p className="text-white text-sm mt-4">The page you requested does not exist</p>
+          <p className="text-white text-sm mt-4">
+            The page you requested does not exist
+          </p>
         </Section>
       </Container>
     </>
