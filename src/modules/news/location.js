97149cf8d7f72cd4df5efa5e87f5dc863f686a81
brdtheo <theo.billardey@gmail.com>
2022-06-28 09:52:14 +0200

    feat: delete news page

--- src/modules/news/location.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import { createLocationTemplate } from "../../lib/redux-saga-router";
-
-export default {
-  news: createLocationTemplate({
-    name: "news",
-    path: "/news",
-  }),
-};
