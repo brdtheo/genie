49bbb5d75cf7efafbab10df3c96b395fc3f32d11
brdtheo <theo.billardey@gmail.com>
2022-06-28 09:52:14 +0200

    feat: delete news page

--- src/locationTemplates.js
+++ src/locationTemplates.js
@@ -2,7 +2,6 @@ import Home from "./modules/home/location";
 import NotFound from './modules/404/location'
 
 export { default as Home } from "./modules/home/location";
-export { default as News } from "./modules/news/location";
 export { default as Search } from "./modules/search/location";
 export { default as NotFound } from "./modules/404/location";
 
