48f3463c41ecfc2f6578cea187bd46ed71a26e74
brdtheo <theo.billardey@gmail.com>
2022-06-28 12:16:54 +0200

    feat: update imports and props

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
+
 import { createRouterReducer } from "../lib/redux-saga-router";
 import { defaultLocation } from "../locationTemplates";
 import homeReducer from "../modules/home/homeReducer";
